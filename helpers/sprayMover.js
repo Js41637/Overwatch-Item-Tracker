465625937c8dc66076daa9422e0863718f70934b
Js41637 ftw <js41637@gmail.com>
2016-12-16 12:19:16 +1100

    dont even try to view this commit

---
+++ helpers/sprayMover.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+
+const checkDirectorys = who => {
+  return new Promise(resolve => {
+    fs.stat(`./${who}`, err => {
+      if (err) {
+        fs.mkdir(`./${who}`, () => {
+          fs.mkdir(`./${who}/sprays`, resolve)
+        })
+      }
+    })
+  })
+}
+
+fs.readdir('./', (err, files) => {
+  files = files.map(f => !f.startsWith('!') && f.endsWith('.png') ? f : null).filter(Boolean)
+  files.forEach(file => {
+    var hero = file.split('-')[0]
+    checkDirectorys(hero).then(() => {
+      fs.rename(`./${file}`, `./${hero}/sprays/${file}`)
+    })
+
+  })
+})
