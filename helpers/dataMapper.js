9a08cb6170c5189e3c4cb364e883bed2bc35df01
Js41637 ftw <js41637@gmail.com>
2017-01-25 21:48:32 +1100

    Add year of the rooster (#12)
    * Add data
    
    * Dis is it bois
    
    * add sprays
    
    * More stuff
    
    * more crap
    
    * Tangerines ?
    
    * Das better
    
    * Finally
    
    * Recalculate costs on Select All
    
    * Oh, calculate per hero progress too
    
    * TAD bigger

--- helpers/dataMapper.js
+++ helpers/dataMapper.js
@@ -183,12 +183,14 @@ var allClassItems = {
   'sprays': {
     [EVENTS.SUMMER16]: ['Summer Games'],
     [EVENTS.HALLOWEEN16]: ['...Never Die', 'Bats', 'Boo!', 'Boop!', 'Candyball', 'Fangs', 'Gummy Hog', 'Halloween Terror 2016', 'Pumpkins', 'Witch\'s Brew'],
-    [EVENTS.CHRISTMAS16]: ['SnowCree', 'SnowHog', 'SnowMei', 'SnowReaper', ['Winter Wonderland']]
+    [EVENTS.CHRISTMAS16]: ['SnowCree', 'SnowHog', 'SnowMei', 'SnowReaper', ['Winter Wonderland']],
+    [EVENTS.ROOSTER17]: ['Auspicious Lion', 'Awakened Lion', ['Dragon\'s Head'], ['Lucky Pouch'], ['Red Envelope'], ['Year of the Rooster']]
   },
   icons: {
     [EVENTS.SUMMER16]: ["Summer Games","Australia", "Brazil", "China", "Egypt", "France", "Germany", "Greece", "Japan", "Mexico", "Nepal", "Numbani", "Russia", "South Korea", "Sweden", "Switzerland", "United Kingdom", "United States"],
     [EVENTS.HALLOWEEN16]: ["Halloween Terror", "...Never Die", "Bewitching", "Calavera", "Candle", "Eyeball", "Ghostymari", "Spider", "Superstition", "Tombstone", "Vampachimari", "Witch's Brew", "Witch's Hat", "Wolf"],
-    [EVENTS.CHRISTMAS16]: ["Winter Wonderland", "Snowman", "Present", "Pachimerry", "Gingermari", "2017", "Holly", "Tannenbaum", "Bubbly", "Gingerbread", "Candy Cane", "Ornament", "Hot Cocoa", "Cheers!", "Wreath", "Mochi", "Dreidel", "Bells", "Peppermint", "Snow Globe", "Pachireindeer", "Stocking"]
+    [EVENTS.CHRISTMAS16]: ["Winter Wonderland", "Snowman", "Present", "Pachimerry", "Gingermari", "2017", "Holly", "Tannenbaum", "Bubbly", "Gingerbread", "Candy Cane", "Ornament", "Hot Cocoa", "Cheers!", "Wreath", "Mochi", "Dreidel", "Bells", "Peppermint", "Snow Globe", "Pachireindeer", "Stocking"],
+    [EVENTS.ROOSTER17]: ["Bokimario", "Coin", "Dragon Dance", "Fortune", "Fuchimari", "Gold", "Have Fish", "Lantern", "Lion Dance", "Lucky Pouch", "Lunamari", "New Year Cake", "Pachilantern", "Red Envelope", "Seollal", "Tangerines", "Year of the Rooster"]
   }
 }
 
@@ -224,6 +226,16 @@ updates[EVENTS.CHRISTMAS16].sprays = updates[EVENTS.CHRISTMAS16].sprays.map(spra
     var ornamentID = `${getCleanID(spray.hero)}-ornament`
     spray.ornamentID = ornamentID;
     spray.ornamentURL = getImageURL('sprays', EVENTS.CHRISTMAS16, ornamentID);
+    return spray
+  } else return spray
+}).filter(Boolean)
+
+// Add dragon dance ids to normal sprays
+updates[EVENTS.ROOSTER17].sprays = updates[EVENTS.ROOSTER17].sprays.map(spray => {
+  if (spray.hero) {
+    var dragonID = `${getCleanID(spray.hero)}-dragon-dance`
+    spray.dragonID = dragonID;
+    spray.dragonURL = getImageURL('sprays', EVENTS.ROOSTER17, dragonID);
     return spray
   } else return spray
 }).filter(Boolean)
