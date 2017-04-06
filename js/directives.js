2ed3fcaa2365744d05f40ed92301682ae0d05730
Js41637 <js41637@gmail.com>
2017-04-06 13:17:54 +1000

    Use a better overwatch loader

--- js/directives.js
+++ js/directives.js
@@ -181,7 +181,7 @@ OWI.directive('loadingSpinner', function() {
     restrict: 'E',
     scope: {},
     replace: true,
-    template: '<div class="loader"><ul class="hexagon-container"><li class="hexagon hex_1"></li><li class="hexagon hex_2"></li><li class="hexagon hex_3"></li><li class="hexagon hex_4"></li><li class="hexagon hex_5"></li><li class="hexagon hex_6"></li><li class="hexagon hex_7"></li></ul></div>'
+    templateUrl: './templates/loader.html'
   }
 })
 
