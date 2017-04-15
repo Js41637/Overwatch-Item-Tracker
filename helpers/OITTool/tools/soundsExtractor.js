0a2814eac44e1e3a92640c58772c0d18aa408680
Js41637 <js41637@gmail.com>
2017-04-15 21:31:35 +1000

    Update soundExtractor

--- helpers/OITTool/tools/soundsExtractor.js
+++ helpers/OITTool/tools/soundsExtractor.js
@@ -8,13 +8,17 @@ const crypto = require('crypto')
 const path = require('path')
 const { exec } = require('child_process')
 
-const { sortBy } = require('lodash')
+const { sortBy, forEach } = require('lodash')
 const { eachLimit } = require('async')
 const moment = require('moment')
 
 const { getDirectories, getCleanID, copyFile, handleErr } = require('./utils')
 const HERODATA = require('../../dataMapper/HERODATA.js')
 const { mapFilesToHeroes } = require('./filesToHeroMapper')
+
+var timestamp = Date.now().toString()
+var existingSounds;
+var existingSoundIDs = {}
 
 const checksum = (str, algorithm, encoding) => {
   return crypto.createHash('sha1').update(str, 'utf8').digest(encoding || 'hex')
@@ -34,7 +38,7 @@ const checkTempDir = () => {
 
 const moveSoundFiles = soundsListOnly => {
   console.log("Mapping and moving sound files")
-  var soundsList = {}, checksumCache = {}, totalFiles = 0, dupeFiles = 0;
+  var soundsList = {}, checksumCache = {}, totalFiles = 0, dupeFiles = 0, totalNewFiles = 0;
   return new Promise(resolve => {
     getDirectories('./').then(heroes => {
       heroes = sortBy(heroes, [h => !Object.keys(HERODATA).includes(getCleanID(h))])
@@ -42,32 +46,41 @@ const moveSoundFiles = soundsListOnly => {
       eachLimit(heroes, 1, (hero, cb) => {
         var heroID = getCleanID(hero)
         soundsList[heroID] = []
+        var newFiles = 0
         getDirectories(`./${hero}`).then(() => {
           getDirectories(`./${hero}/Sound Dump`).then(sounds => {
             Promise.all(sounds.filter(a => a.endsWith('.wem')).map(sound => {
               return new Promise(r => {
                 totalFiles++;
                 const checksum = getFileSize(`./${hero}/Sound Dump/${sound}`)
-                var dupeFile = checksum in checksumCache
+                const dupeFile = checksum in checksumCache ? { dupe: true } : undefined
+                const id = sound.replace('.wem', '')
+                const oldSoundTS = (existingSoundIDs[id] && existingSoundIDs[id].length) ? { ts: existingSoundIDs[id] } : undefined
+                const isNew = !existingSoundIDs[id] ? { ts: timestamp } : undefined
                 soundsList[heroID].push(Object.assign({}, {
-                  id: sound.replace('.wem', ''),
+                  id: id,
                   checksum: checksum
-                }, dupeFile ? { dupe: true } : {}))
+                }, dupeFile, isNew, oldSoundTS))
                 checksumCache[checksum] = true
                 if (dupeFile) {
                   dupeFiles++;
                   return r()
                 }
-                if (soundsListOnly) return r()
+                if (isNew) newFiles++
+                if (soundsListOnly || !isNew) return r()
                 copyFile(`./${hero}/Sound Dump/${sound}`, `./!soundTemp/${heroID}-${sound}`, r)
               })
             })).then(() => {
               console.log("- Found", sounds.length, "sounds for", hero)
+              if (newFiles) {
+                console.log("-- Found", newFiles, "new sounds for", hero)
+                totalNewFiles += newFiles
+              }
               cb()
             })
           }).catch(err => console.error("- Error getting sounds for", hero, err))
         }).catch(err => console.error("- Error loading hero dir", hero, err))
-      }, () => resolve({ soundsList, checksumCache, totalFiles, dupeFiles }))
+      }, () => resolve({ soundsList, checksumCache, totalFiles, dupeFiles, totalNewFiles }))
     })
   })
 }
@@ -103,14 +116,25 @@ const extractSounds = args => {
     process.exit()
   }
 
+  try {
+    existingSounds = JSON.parse(fs.readFileSync('./soundFiles.json', 'utf8'))
+  } catch(e) {
+    console.warn("Couldn't find existing sounds file?")
+  }
+
+  if (existingSounds) {
+    forEach(existingSounds, sounds => forEach(sounds, sound => existingSoundIDs[sound.id] = sound.ts || true))
+  }
+
   var soundsListOnly = args[0]
   const startTS = Date.now()
 
   checkTempDir()
-  moveSoundFiles(soundsListOnly).then(({ soundsList, checksumCache, totalFiles, dupeFiles }) => { //eslint-disable-line
+  moveSoundFiles(soundsListOnly).then(({ soundsList, checksumCache, totalFiles, dupeFiles, totalNewFiles }) => { //eslint-disable-line
     console.log("Done generating and moving sound data")
     console.log("- Mapped", totalFiles, "sound files")
     console.log("- Detected", dupeFiles, "dupe files")
+    console.log("- Detected", totalNewFiles, "new files")
     fs.writeFileSync('./soundFiles.json', JSON.stringify(soundsList, null, 2))
     if (soundsListOnly) return
 
