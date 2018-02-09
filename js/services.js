354223d6724efac1608aa85ab74ba400c955e1bf
Js41637 <Js41637@users.noreply.github.com>
2018-02-09 22:11:32 +1100

    God dammit

--- js/services.js
+++ js/services.js
@@ -135,7 +135,7 @@ OWI.factory('CostAndTotalService', ["DataService", "StorageService", "$q", "$tim
     totals: {},
     heroes: {},
     events: {},
-    oldEvents: ['HALLOWEEN_2016', 'SUMMER_GAMES_2016', 'WINTER_WONDERLAND_2016'],
+    oldEvents: ['HALLOWEEN_2016', 'SUMMER_GAMES_2016', 'WINTER_WONDERLAND_2016', 'LUNAR_NEW_YEAR_2017'],
     newEvents: ['HALLOWEEN', 'SUMMER_GAMES', 'WINTER_WONDERLAND', 'LUNAR_NEW_YEAR'],
     init: function() {
       DataService.waitForInitialization().then(function() {
