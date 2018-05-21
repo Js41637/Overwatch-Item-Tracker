3ab0224bc04ee79d0abbbaa913f413f9e26b9e83
Js41637 <Js41637@users.noreply.github.com>
2018-05-21 20:33:44 +1000

    More gulp stuff

--- gulpfile.js
+++ gulpfile.js
@@ -3,21 +3,25 @@ const concat = require('gulp-concat')
 const sass = require('gulp-sass')
 const minifyCSS = require('gulp-csso')
 const watch = require('gulp-watch')
+const plumber = require('gulp-plumber');
 
 gulp.task('css', function() {
   gulp.src(['css/*.scss', 'css/!global.scss'])
+    .pipe(plumber())
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(concat('main.min.css'))
     .pipe(gulp.dest('css'))
 
   gulp.src('css/global.scss')
+    .pipe(plumber())
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(concat('global.min.css'))
     .pipe(gulp.dest('css'))
 
   gulp.src('css/themes/dirtdiglett/*.scss')
+    .pipe(plumber())
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(concat('main.min.css'))
