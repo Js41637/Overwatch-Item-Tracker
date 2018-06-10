76641822f4627021c852e2f4974717eb3a8b1ea0
Js41637 <Js41637@users.noreply.github.com>
2018-06-10 21:10:33 +1000

    Merge Offence & Defence classes into Damage

--- helpers/dataMapper/HERODATA.js
+++ helpers/dataMapper/HERODATA.js
@@ -1,6 +1,5 @@
 const CLASSES = {
-  OFFENCE: 'Offense',
-  DEFENCE: 'Defense',
+  DAMAGE: 'Damage',
   TANK: 'Tank',
   SUPPORT: 'Support'
 };
@@ -21,7 +20,7 @@ const data = {
     }
   },
   bastion: {
-    class: CLASSES.DEFENCE,
+    class: CLASSES.DAMAGE,
     description: "Once a frontline combatant in the Omnic Crisis, this curious Bastion unit now explores the world, fascinated by nature but wary of a fearful humanity.",
     colors: {
       primary: "#6e994d",
@@ -37,7 +36,7 @@ const data = {
     }
   },
   doomfist: {
-    class: CLASSES.OFFENCE,
+    class: CLASSES.DAMAGE,
     description: "One of the leaders of Talon, Doomfist is determined to plunge the world into a new conflict that he believes will make humanity stronger.",
     colors: {
       primary: "#e04e34",
@@ -53,7 +52,7 @@ const data = {
     }
   },
   genji: {
-    class: CLASSES.OFFENCE,
+    class: CLASSES.DAMAGE,
     description: "Genji Shimada has made peace with the cyborg body he once rejected, and in doing so, he has discovered a higher humanity.",
     colors: {
       primary: "#84fe01",
@@ -61,7 +60,7 @@ const data = {
     }
   },
   hanzo: {
-    class: CLASSES.DEFENCE,
+    class: CLASSES.DAMAGE,
     description: "Mastering his skills as a bowman and an assassin, Hanzo Shimada strives to prove himself as a warrior without peer.",
     colors: {
       primary: "#938848",
@@ -69,7 +68,7 @@ const data = {
     }
   },
   junkrat: {
-    class: CLASSES.DEFENCE,
+    class: CLASSES.DAMAGE,
     description: "Junkrat is an explosives-obsessed freak who lives to cause chaos and destruction.",
     colors: {
       primary: "#d39308 ",
@@ -85,7 +84,7 @@ const data = {
     }
   },
   mccree: {
-    class: CLASSES.OFFENCE,
+    class: CLASSES.DAMAGE,
     description: "Armed with his Peacekeeper revolver, the outlaw Jesse McCree doles out justice on his own terms.",
     colors: {
       primary: "#8d3939",
@@ -93,7 +92,7 @@ const data = {
     }
   },
   mei: {
-    class: CLASSES.DEFENCE,
+    class: CLASSES.DAMAGE,
     description: "Mei is a climatologist who has taken the fight to preserve the environment and protect the world into her own hands.",
     colors: {
       primary: "#9adbf4",
@@ -125,7 +124,7 @@ const data = {
     }
   },
   pharah: {
-    class: CLASSES.OFFENCE,
+    class: CLASSES.DAMAGE,
     description: "Pharah's commitment to duty runs in her blood. She comes from a long line of highly decorated soldiers and burns with the desire to serve with honor.",
     colors: {
       primary: "#1b65c6",
@@ -133,7 +132,7 @@ const data = {
     }
   },
   reaper: {
-    class: CLASSES.OFFENCE,
+    class: CLASSES.DAMAGE,
     description: "Some speak of a black-robed terrorist known only as the Reaper. His identity and motives are a mystery. What is known is that where he appears, death follows.",
     colors: {
       primary: "#272725",
@@ -157,7 +156,7 @@ const data = {
     }
   },
   "soldier-76": {
-    class: CLASSES.OFFENCE,
+    class: CLASSES.DAMAGE,
     description: "Currently the target of an international manhunt, the vigilante known as Soldier: 76 wages a personal war to expose the truth behind Overwatch's collapse.",
     colors: {
       primary: "#5870b6",
@@ -165,7 +164,7 @@ const data = {
     }
   },
   sombra: {
-    class: CLASSES.OFFENCE,
+    class: CLASSES.DAMAGE,
     description: "One of the world's most notorious hackers, Sombra uses information to manipulate those in power.",
     colors: {
       primary: "#751b9c",
@@ -173,7 +172,7 @@ const data = {
     }
   },
   symmetra: {
-    class: CLASSES.SUPPORT,
+    class: CLASSES.DAMAGE,
     description: "Symmetra literally bends reality. By manipulating hard-light constructs, she crafts the world as she wishes it to be, in hopes of engineering a perfect society.",
     colors: {
       primary: "#5cecff",
@@ -181,7 +180,7 @@ const data = {
     }
   },
   torbjorn: {
-    class: CLASSES.DEFENCE,
+    class: CLASSES.DAMAGE,
     description: "At its height, Overwatch possessed one of the most advanced armaments on the planet, which could be traced to the workshop of an ingenious engineer named Torbjörn Lindholm.",
     colors: {
       primary: "#ff6200",
@@ -189,7 +188,7 @@ const data = {
     }
   },
   tracer: {
-    class: CLASSES.OFFENCE,
+    class: CLASSES.DAMAGE,
     description: "The former Overwatch agent known as Tracer is a time-jumping adventurer and an irrepressible force for good.",
     colors: {
       primary: "#f8911b",
@@ -197,7 +196,7 @@ const data = {
     }
   },
   widowmaker: {
-    class: CLASSES.DEFENCE,
+    class: CLASSES.DAMAGE,
     description: "Widowmaker is the perfect assassin: a patient, ruthlessly efficient killer who shows neither emotion nor remorse.",
     colors: {
       primary: "#6f6fae",
