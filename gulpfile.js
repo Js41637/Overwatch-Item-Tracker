61033b7e534f69c9a233ed3ea610a0d84cf57537
Js41637 <Js41637@users.noreply.github.com>
2018-12-14 16:59:59 +1100

    update some deps

--- gulpfile.js
+++ gulpfile.js
@@ -2,27 +2,28 @@ const gulp = require('gulp')
 const concat = require('gulp-concat')
 const sass = require('gulp-sass')
 const minifyCSS = require('gulp-csso')
-const watch = require('gulp-watch')
-const plumber = require('gulp-plumber');
+const plumber = require('gulp-plumber')
 
-gulp.task('css', function() {
-  gulp.src('css/main.scss',)
+function css() {
+  gulp
+    .src('css/main.scss',)
     .pipe(plumber())
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(concat('main.min.css'))
     .pipe(gulp.dest('css'))
 
-  gulp.src('css/themes/dirtdiglett/*.scss')
+  return gulp
+    .src('css/themes/dirtdiglett/*.scss')
     .pipe(plumber())
     .pipe(sass())
     .pipe(minifyCSS())
     .pipe(concat('main.min.css'))
     .pipe(gulp.dest('css/themes/dirtdiglett'))
-})
+}
+
+gulp.task('css', css)
 
 gulp.task('dev', function() {
-  return watch('css/**/*.scss', function() {
-    gulp.start('css')
-  })
+  gulp.watch('css/**/*.scss', css)
 })
